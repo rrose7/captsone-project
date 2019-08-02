3f72bf4db1eb8a1e1cc81f6c304ff2d6dc22535f
rrose7 <rdhakal77@gmail.com>
2019-08-02 12:29:48 -0500

    Updated Category

--- suchana-ui/src/app/components/category/edit-category/edit-category.component.spec.ts
+++ suchana-ui/src/app/components/category/edit-category/edit-category.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CategoryComponent } from './category.component';
+import { EditCategoryComponent } from './edit-category.component';
 
-describe('CategoryComponent', () => {
-  let component: CategoryComponent;
-  let fixture: ComponentFixture<CategoryComponent>;
+describe('EditCategoryComponent', () => {
+  let component: EditCategoryComponent;
+  let fixture: ComponentFixture<EditCategoryComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CategoryComponent ]
+      declarations: [ EditCategoryComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CategoryComponent);
+    fixture = TestBed.createComponent(EditCategoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- suchana-ui/src/app/services/category.service.ts
+++ suchana-ui/src/app/services/category.service.ts
@@ -1,15 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  API_BASE_URL ="http://localhost:8080";
 
-  constructor(private http:HttpClient) { }
-  getCategories(): Observable<any>{
-    return this.http.get(this.API_BASE_URL +"/categories");
-  }
+  constructor() { }
 }
